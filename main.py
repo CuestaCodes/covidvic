659f484a6ba801ac26502b08744fbc07a8e87375
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-02-11 22:01:48 +1100

    single page scrape

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 if __name__ == "__main__":
     from bs4 import BeautifulSoup
-    print("Hello World!")
+    from urllib.request import urlopen
+
+    link = "https://www.health.vic.gov.au/media-releases/coronavirus-update-for-victoria-9-february-2022"
+    soup = BeautifulSoup(urlopen(link))
+
+    print(soup.prettify())
