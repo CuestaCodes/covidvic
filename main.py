9bd46181b4ad8d53252d9e84b4fd4a354c954151
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-02-12 21:12:58 +1100

    working loop for date scrape

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ if __name__ == "__main__":
     from bs4 import BeautifulSoup
     from urllib.request import urlopen
 
-    months = ["january", "february", "march", "april", "may", "june",
-              "july", "august", "september", "october", "november", "december"]
-    years = ["2019", "2020", "2019"]
+    months = ["january-", "february-", "march-", "april-", "may-", "june-",
+              "july-", "august-", "september-", "october-", "november-", "december-"]
+    years = ["2021", "2022"]
 
     days = []
     for num in range(1, 32):
-        days.append(str(num))
+        days.append(str(num) + "-")
 
-    link = "https://www.health.vic.gov.au/media-releases/coronavirus-update-for-victoria-9-february-2022"
-    soup = BeautifulSoup(urlopen(link))
+    # "https://www.health.vic.gov.au/media-releases/coronavirus-update-for-victoria-9-february-2022"
+    link_prefix = "https://www.health.vic.gov.au/media-releases/coronavirus-update-for-victoria-"
 
-    print(soup.prettify())
+    for year in years:
+        for month in months:
+            for day in days:
+                link = link_prefix + day + month + year
+
+                try:
+                    soup = BeautifulSoup(urlopen(link))
+                    print(soup.prettify())
+                except:
+                    print(link)
+                    continue
